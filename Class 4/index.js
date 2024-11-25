fc13c8c5eeb5f222f641753677cf14c44646e474
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-25 09:20:02 -0300

    Class 4

---
+++ Class 4/index.js
@@ -0,0 +1,16 @@
+let nome1 = 'João' // var ou let
+let nome2 = 'Maria' 
+let anos = [1984, 2000, 2012, 2015] 
+
+
+console.log(`${nome1} nasceu em ${anos[0]}.`)
+console.log(`Em ${anos[1]} ${nome1} conheceu ${nome2}.`)
+console.log(`${nome1} casou-se com ${nome2} em ${anos[2]}.`)
+console.log(`${nome2} teve 1 filho com ${nome1} em ${anos[3]}.`)
+console.log(`O filho de ${nome1} se chama Eduardo.`)
+
+let dia
+dia = 25
+console.log(`Hoje é dia ${dia}`)
+dia = 26
+console.log(`Hoje é dia ${dia}`)
