5f30a7e87a1d6bdf4e4efd64d1f9d7229d6bdfed
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-25 16:32:03 -0300

    Class 9

---
+++ Class 9/index.js
@@ -0,0 +1,15 @@
+let varA = 'A' // B
+let varB = 'B' // C
+let varC = 'C' // A
+
+//[varA, varB, varC] = [varB, varC, varA]
+
+let vari = ['A','B','C']
+
+
+varA = vari[1]
+varB = vari[2]
+varC = vari[0]
+
+
+console.log(varA, varB, varC)
