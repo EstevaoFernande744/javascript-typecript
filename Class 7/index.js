56cb6a99ba45e0c0d9f44aeba8aa66df556720d1
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-25 11:55:05 -0300

    Class 7

---
+++ Class 7/index.js
@@ -0,0 +1,22 @@
+/*
+* Aritméticos
+* + Adição / Concatenação
+* - Subtração
+* / Divisão
+* * Multiplicação
+* ** Potenciação
+* % Resto da divisão
+*/
+
+const num1 = 5
+const num2 = 10
+const num3 = 3
+
+console.log(num1 + num2)
+console.log(num2 % num3)
+
+//ordem de operação () => ** => *, /, % => +, - 
+
+let contador = 1
+contador ++ // contador = contador + 1
+console.log(contador)
