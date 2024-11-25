39c689bcc0bf3bd7a87083e573ebd2a95ebeecf6
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-25 16:12:33 -0300

    Class 8

---
+++ Class 8/index.js
@@ -0,0 +1,16 @@
+//alert('Seja bem vindo!.')
+//let feliz = window.confirm('Você esta feliz?')
+//let nome = window.prompt('Digite o seu nome!')
+
+//console.log(feliz)
+//console.log(nome)
+
+let num1 = Number(prompt('Digite um número'))
+
+let num2 = Number(prompt('Digite outro número'))
+
+let soma = num1 + num2 
+
+alert(`O resultado da soma de ${num1} + ${num2} é ${soma}`)
+
+console.log(`O resultado da soma de ${num1} + ${num2} é ${soma}`) // Podemos também colocar ${num1 + num2}
