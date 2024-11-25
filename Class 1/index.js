caecbf840fbdf101ef4505f64a1842e9c0615839
Estevão Fernandes <estevaofernandes744@gmail.com>
2024-11-24 23:24:13 -0300

    class 1

--- Class 1/index.js
+++ Class 1/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+console.log('Hello World'); //String = Texto
+console.log("Hello World");
+console.log(`Hello World`)
+/*caso eu queira dizer que World esta entre aspas podemos usar
+console.log("Hello 'World'")
+ou console.log('Hello "world"')
+dessas duas formas seram expresso o termo World entre aspas
+*/
+
+console.log(123456, 15.85, 'Hello World') // a virgula me permite colocar mais de uma informação
+
+// console.log(12) // Number = Número
