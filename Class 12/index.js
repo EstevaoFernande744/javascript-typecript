6c42a1dd246a16ece2558032a53d91aa8944fae5
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-26 09:28:16 -0300

    Class 12

---
+++ Class 12/index.js
@@ -0,0 +1,28 @@
+let num1 = 9.54578
+let num2 = Math.floor(num1)
+
+console.log(num2)
+
+num2 = Math.ceil(num1)
+
+console.log(num2)
+
+num2 = Math.round(num1)
+
+console.log(num2)
+
+num2 = Math.max(1, 2, 3, 4, 19, -10, -2, 0)
+
+console.log(num2)
+
+num2 = Math.min(1, 2, 3, 4, 19, -10, -2, 0)
+
+console.log(num2)
+
+console.log(Math.random())
+
+let aleatorio = Math.random() * (10 - 5) + 5 // Número aleatorio entre 10 e 5
+
+console.log(num1 ** (1/2)) // Raiz quadrada
+
+// Ou console.log(num1 ** 0.5)
