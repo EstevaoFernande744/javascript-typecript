ffe1f8f26934a3ec72556dcd356fe97c7fdb6b96
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-11-28 17:57:00 -0300

    modelo 2 incomplet

---
+++ modelo2/js/script.js
@@ -0,0 +1,32 @@
+// Capturar     
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    console.log('Evento Previnido')
+    setResultado('Olá Mundo!')
+})
+
+function setResultado(msg){ // função para escrever o resultado
+    const resultado = document.querySelector('#resultado') /* esta pegando o id resultado */
+    resultado.innerHTML = msg /* estou dizendo que minha variavel resultado vai fazer uma mensagem*/
+}
+
+/*
+- esse form. = elemento do formulario
+- addEventListener('evento que sera ouvido',  função que vai acontecer ao ser chamada())
+- function(event) é uma função com o parametro evento 
+- event.preventDefault() serve para previnir que o evento aconteça.
+*/
+
+/*
+let peso = document.getElementById('ipeso')
+let altura = document.getElementById('ialtura')
+
+function calculoIMC(){
+    let IMC = peso / (altura ** 2)
+    return 
+}
+
+*/
