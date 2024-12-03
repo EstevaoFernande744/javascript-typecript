8cae60cae23af7982ce6e4f72ba17f6f2c3b58dd
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-03 11:02:48 -0300

    Class 35

---
+++ Class 35/script.js
@@ -0,0 +1,39 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles;
+
+
+function max(a, b){
+return a > b ? a : b;
+}
+console.log(max(12, 10));
+
+
+const max2 = (x,y) => {
+    return x > y ? x : y;
+};
+
+console.log(max2(20, 199))
+
+
+function comparacao(a, b){
+
+    /*
+    if(a > b){
+       `O ${a} é maior do que o ${b}`
+    } else {
+       `O ${b} é maior do que o ${a}`
+    }
+    */
+   
+    // usando o operador ternario
+
+    return a > b ? `O valor ${a} é maior do que o ${b}` : `O  valor ${b} é maior do que o ${a}`
+}
+
+console.log(comparacao(12, 10));
+
+
+// solução com arrowfunction simples
+
+const max3 = (x, y) => x > y ? x : y;
+
+console.log(max3(50, 213))
