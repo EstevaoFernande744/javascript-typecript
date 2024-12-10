920bdedf709c7c00d4be2f86db7c1a4fb5a75753
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-10 08:28:54 -0300

    Class 61

--- Class 61/script.js
+++ Class 61/script.js
@@ -11,6 +11,37 @@ function Produto(nome, preço, estoque, marca) {
         // writable: false  // não pode mudar o valor do estoque
         // configurable: true, // pode apagar a chave? ou reconfigurar? 
     });
+
+    Object.defineProperty(this, 'estoque', { 
+        enumerable: true, // mostrar a chave
+        value: estoque, // valor
+        writable: false, // pode alterar
+        configurable: false // configurar
+    })
+
+    Object.defineProperties(this, {
+        
+        nome: {
+            enumerable: true, 
+            value: estoque, 
+            writable: false, 
+            configurable: false
+        },
+
+        preço: {
+            enumerable: true, 
+            value: estoque, 
+            writable: false, 
+            configurable: false
+        },
+
+        marca: {
+            enumerable: true, 
+            value: estoque, 
+            writable: false, 
+            configurable: false
+        },
+    })
 }
 
 const produto1 = new Produto('Camiseta', 45, 100, 'Ditongo');
@@ -18,5 +49,12 @@ const produto2 = new Produto('Calça', 100, 50, 'Sarwel');
 const produto3 = new Produto('Moletom', 120, 80, 'Manikas');
 const produto4 = new Produto('Boné', 30, 100, 'Nike');
 const produto5 = new Produto('Tenis', 290, 40, 'Addidas');
+produto2.estoque = 5000 // se o configurable for true esse valor vai alterar o estoque
+delete produto2.estoque; 
 
 console.log(produto2)
+console.log(Object.keys(produto2));
+
+for(let chave in produto2) {
+    console.log(chave);
+}
